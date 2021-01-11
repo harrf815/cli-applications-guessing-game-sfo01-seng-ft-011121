bfed7c71dee9b490afa5d5e0740a61490315ef85
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-11 05:38:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ require_relative "./spec/spec_helper.rb"
 def run_guessing_game
   puts "/Guess a number between 1 and 6."
   number = rand(6) + 1 
-  user_input = gets.chomp.to_s
+  user_input = gets.chomp
     if user_input == number 
       return "You guessed the correct number!"
     elsif user_input == number
