18b430f0949341465002985f17ecb1a82b7e298a
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-11 05:45:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,16 +1,18 @@
 # Code your solution here!
 require_relative "./spec/spec_helper.rb"
+require 'pry'
 
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   number = rand(6) + 1 
-  user_input = gets.chomp.to_s
+  user_input = gets.chomp
     if user_input == number 
       puts "You guessed the correct number!"
     elsif user_input != number
       puts "Sorry! The computer guessed #{number}."
     else user_input == "exit"
       puts "Goodbye!"
+      binding.pry
     end
 
 end
