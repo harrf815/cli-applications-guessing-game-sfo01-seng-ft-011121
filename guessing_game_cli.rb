b01d9eb126cf5b4de40a8b4753e6a7e70ad3c252
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-11 05:37:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,2 +1,16 @@
 # Code your solution here!
 require_relative "../guessing_game_cli"
+
+def run_guessing_game
+  puts "/Guess a number between 1 and 6."
+  number = rand(6) + 1 
+  user_input = gets.chomp
+    if user_input == roll 
+      return "You guessed the correct number!"
+    elsif user_input == roll
+      return "Sorry! The computer guessed #{number}."
+    else user_input == "exit"
+      puts "Goodbye!"
+    end
+
+end
